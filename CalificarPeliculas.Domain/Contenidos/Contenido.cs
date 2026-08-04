7efcc27490944dca7daa3e0dfba51ac1724191dc
MarthQ <martinez34950@gmail.com>
2026-08-03 21:50:05 -0300

    Added folder structure to the Solution, established two potential classes for first CRUDs

---
+++ CalificarPeliculas.Domain/Contenidos/Contenido.cs
@@ -0,0 +1,16 @@
+﻿namespace CalificarPeliculas.Domain.Contenidos
+{
+    public class Contenido
+    {
+        private int id;
+        private int idTMDB;
+        public string tipo;
+        public string nombre;
+        public string descripcion;
+        public string nombreDirector;
+        public DateOnly fechaLanzamiento;
+        public TimeOnly duracion;
+        public string urlImagen;
+        public float puntuacionPromedio;
+    }
+}
